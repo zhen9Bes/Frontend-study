cc36ac05a26203d5d0c76924bc008a11889827be
Jacky <jorik18901@gmail.com>
2020-08-25 23:48:06 +0700

    Add Our trip section

--- From0To1/Cappadocia/js/main.js
+++ From0To1/Cappadocia/js/main.js
@@ -6,4 +6,11 @@ $(function () {
         autoplaySpeed: 3000,
         fade: true
     })
+    $('.our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="img/arrow-prew.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="img/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true
+    })
 });
