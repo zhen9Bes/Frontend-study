74a380f06ff0ed33d51f79a705dbfac08d4748d2
Jacky <jorik18901@gmail.com>
2020-08-27 01:37:51 +0700

    Add adaptive for interface

--- From0To1/Cappadocia/js/main.js
+++ From0To1/Cappadocia/js/main.js
@@ -4,13 +4,29 @@ $(function () {
         nextArrow: '<button type="button" class="slick-next slick-btn"><img src="img/arrow-next.svg" alt=""></button>',
         autoplay: true,
         autoplaySpeed: 3000,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 601,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     })
     $('.our-trip__slider').slick({
         prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="img/arrow-prew.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"><img src="img/arrow-next.svg" alt=""></button>',
         autoplay: true,
         autoplaySpeed: 3000,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 601,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     })
 });
