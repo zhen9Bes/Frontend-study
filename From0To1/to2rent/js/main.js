9b87e6c1020da90c2fb4c5c78d663261a64ce82b
Jacky <jorik18901@gmail.com>
2020-08-28 23:21:21 +0700

    Start adaptive for header and slider

--- From0To1/to2rent/js/main.js
+++ From0To1/to2rent/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
     $('.slider').slick({
         prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/prev.svg" alt=""></button>',
-        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/next.svg" alt=""></button>'
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/next.svg" alt=""></button>',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive:[
+            {
+                breackpoint: 768,
+                arrows: false
+            }
+        ]
     })
 });
