5efd5e2cf2de08e2be13fb4660a4c0b6aff13472
Jacky <jorik18901@gmail.com>
2020-08-28 22:41:14 +0700

    Add slider section

--- From0To1/to2rent/js/main.js
+++ From0To1/to2rent/js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-
+$(function () {
+    $('.slider').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/prev.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/next.svg" alt=""></button>'
+    })
 });
